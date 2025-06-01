0536a014aba926a6b1bb602d4598d848e87b7ade
harpreetsinghjk99-collab <harpreetsinghjk99@gmail.com>
2025-06-01 20:36:11 +0100

    first day lessons

---
+++ Python_comment.py
@@ -0,0 +1,18 @@
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code.
+
+print("Hello, World!") #This is a comment at the end of a line
+
+#Below comment is to prevent the code from excecuting
+#print("Hello, World!") 
+
+#Python will ignore string literals that are not assigned to a variable,
+# you can add a multiline string (triple quotes) in your code,
+# and place your comment inside it
+
+'''
+ Python will ignore string literals that are not assigned to a variable,
+ you can add a multiline string (triple quotes) in your code,
+ and place your comment inside it
+'''
