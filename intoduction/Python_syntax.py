cdbff33371885373dbfb5e47c67499ba974fa390
harpreetsinghjk99-collab <harpreetsinghjk99@gmail.com>
2025-06-01 20:45:10 +0100

    introduction files

---
+++ intoduction/Python_syntax.py
@@ -0,0 +1,10 @@
+#Python use indentation to indicate a block of code
+#indentation refer to the space in the begining of the code line. 
+if 2 > 3: 
+    print ("2 is greater than 3")
+else:
+    print("3 is greater than 2")
+
+#Python will give error if the you skip the indentation
+#Can use same no of spaces otherwise pyhton will thrown an error
+#It is recomended to use tab insread (equivalent to 4 spaces)
---
+++ intoduction/Python_version.py
@@ -0,0 +1,4 @@
+import sys
+
+print("The verison of python is:")
+print(sys.version)
